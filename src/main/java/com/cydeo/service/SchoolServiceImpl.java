a865fec04f3f1a7f278b2a186bb702c524689a25
stanislavpavlovskyi <lichfb@gmail.com>
2023-02-14 11:14:34 -0500

    School Service Impl Created

---
+++ src/main/java/com/cydeo/service/SchoolServiceImpl.java
@@ -0,0 +1,32 @@
+package com.cydeo.service;
+
+import com.cydeo.entity.School;
+
+import java.util.List;
+
+public class SchoolServiceImpl implements CRUDService<School>{
+    @Override
+    public School findById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<School> findAll() {
+        return null;
+    }
+
+    @Override
+    public void save(School school) {
+
+    }
+
+    @Override
+    public void update(School school) {
+
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+}
