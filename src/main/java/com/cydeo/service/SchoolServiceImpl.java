1be5c5b24ac465d02ef269c3376369243627d9d7
stanislavpavlovskyi <lichfb@gmail.com>
2023-02-14 20:32:51 -0500

    FindById and FindAll methods added

--- src/main/java/com/cydeo/service/SchoolServiceImpl.java
+++ src/main/java/com/cydeo/service/SchoolServiceImpl.java
@@ -1,18 +1,27 @@
 package com.cydeo.service;
 
+import com.cydeo.database.Database;
 import com.cydeo.entity.School;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+
+import static com.cydeo.database.Database.schoolList;
 
 public class SchoolServiceImpl implements CRUDService<School>{
     @Override
     public School findById(int id) {
-        return null;
+        try{
+            return schoolList.stream().filter(school -> school.getId() == id).findAny().orElseThrow();
+        }catch(NoSuchElementException e){
+            return new School();
+        }
     }
 
     @Override
     public List<School> findAll() {
-        return null;
+        return schoolList.stream().collect(Collectors.toList());
     }
 
     @Override
