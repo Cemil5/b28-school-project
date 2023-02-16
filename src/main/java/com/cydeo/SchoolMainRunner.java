4d7b582c580d6ea8ce9b663706740527671d074b
stanislavpavlovskyi <lichfb@gmail.com>
2023-02-15 22:39:15 -0500

    ...

--- src/main/java/com/cydeo/SchoolMainRunner.java
+++ src/main/java/com/cydeo/SchoolMainRunner.java
@@ -1,27 +1,30 @@
 package com.cydeo;
 
 import com.cydeo.database.Database;
-import com.cydeo.entity.School;
+import com.cydeo.entity.*;
 import com.cydeo.service.CRUDService;
 import com.cydeo.service.SchoolServiceImpl;
 
 public class SchoolMainRunner {
 
-//    public static CRUDService<School> schoolService;
-//    public static CRUDService<Course> courseService;
-//    public static CRUDService<Parent> parentService;
-//    public static CRUDService<Student> studentService;
-//
-//    static {
-//        SchoolMainRunner.schoolService = new SchoolService();
-//        SchoolMainRunner.courseService = new CourseService();
-//        SchoolMainRunner.parentService = new ParentService();
-//        SchoolMainRunner.studentService = new StudentService();
-//    }
+  /* public static CRUDService<School> schoolService;
+    public static CRUDService<Course> courseService;
+    public static CRUDService<Parent> parentService;
+    public static CRUDService<Student> studentService;
+
+    static {
+        SchoolMainRunner.schoolService = new SchoolServiceImpl();
+        SchoolMainRunner.courseService = new CourseService();
+        SchoolMainRunner.parentService = new ParentService();
+        SchoolMainRunner.studentService = new StudentService();
+    }*/
 
     public static void main(String[] args) {
 
-        Database.courseList.forEach(System.out::println);
+       // Database.courseList.forEach(System.out::println);
+
+
+
 
 
 
