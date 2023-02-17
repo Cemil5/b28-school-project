aa8d6059b56e882a01c1571f1ffa79385b9de6ff
Cemil <chrisw@cydeo.com>
2023-02-17 20:13:35 +0300

    refactor

--- src/main/java/com/cydeo/SchoolMainRunner.java
+++ src/main/java/com/cydeo/SchoolMainRunner.java
@@ -1,33 +1,24 @@
 package com.cydeo;
 
 import com.cydeo.database.Database;
-import com.cydeo.entity.*;
-import com.cydeo.service.CRUDService;
-import com.cydeo.service.TeacherService;
 
 public class SchoolMainRunner {
 
-    public static CRUDService<School> schoolService;
-    public static CRUDService<Course> courseService;
-    public static CRUDService<Parent> parentService;
-    public static CRUDService<Student> studentService;
-    public static CRUDService<Teacher> teacherService;
-
-    static {
+//    public static CRUDService<School> schoolService;
+//    public static CRUDService<Course> courseService;
+//    public static CRUDService<Parent> parentService;
+//    public static CRUDService<Student> studentService;
+//
+//    static {
 //        SchoolMainRunner.schoolService = new SchoolService();
 //        SchoolMainRunner.courseService = new CourseService();
 //        SchoolMainRunner.parentService = new ParentService();
 //        SchoolMainRunner.studentService = new StudentService();
-        teacherService = new TeacherService();
-    }
+//    }
 
     public static void main(String[] args) {
-        try {
-            System.out.println(teacherService.findById(3));
 
-        } catch (Exception e){
-            System.out.println(e.getMessage());
-        }
+        Database.courseList.forEach(System.out::println);
 
     }
 
