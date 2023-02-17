6d0f1ce0c587426bed2b551af5e58a48124b9d43
Cemil <chrisw@cydeo.com>
2023-02-17 20:00:15 +0300

    added teacher service implementation

--- src/main/java/com/cydeo/SchoolMainRunner.java
+++ src/main/java/com/cydeo/SchoolMainRunner.java
@@ -1,24 +1,33 @@
 package com.cydeo;
 
 import com.cydeo.database.Database;
+import com.cydeo.entity.*;
+import com.cydeo.service.CRUDService;
+import com.cydeo.service.TeacherService;
 
 public class SchoolMainRunner {
 
-//    public static CRUDService<School> schoolService;
-//    public static CRUDService<Course> courseService;
-//    public static CRUDService<Parent> parentService;
-//    public static CRUDService<Student> studentService;
-//
-//    static {
+    public static CRUDService<School> schoolService;
+    public static CRUDService<Course> courseService;
+    public static CRUDService<Parent> parentService;
+    public static CRUDService<Student> studentService;
+    public static CRUDService<Teacher> teacherService;
+
+    static {
 //        SchoolMainRunner.schoolService = new SchoolService();
 //        SchoolMainRunner.courseService = new CourseService();
 //        SchoolMainRunner.parentService = new ParentService();
 //        SchoolMainRunner.studentService = new StudentService();
-//    }
+        teacherService = new TeacherService();
+    }
 
     public static void main(String[] args) {
+        try {
+            System.out.println(teacherService.findById(3));
 
-        Database.courseList.forEach(System.out::println);
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
 
     }
 
