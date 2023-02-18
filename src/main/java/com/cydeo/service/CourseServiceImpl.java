6f030d79e424c85026052e5c771866d3b2a12a0f
Sasha <alexandra.aslobodian@gmail.com>
2023-02-17 19:30:13 -0500

    created new branch and extended CRUD interface

---
+++ src/main/java/com/cydeo/service/CourseServiceImpl.java
@@ -0,0 +1,33 @@
+package com.cydeo.service;
+
+import com.cydeo.entity.Course;
+
+import java.util.List;
+
+public class CourseServiceImpl implements CRUDService<Course> {
+
+    @Override
+    public Course findById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Course> findAll() {
+        return null;
+    }
+
+    @Override
+    public void save(Course course) {
+
+    }
+
+    @Override
+    public void update(Course course) {
+
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+}
