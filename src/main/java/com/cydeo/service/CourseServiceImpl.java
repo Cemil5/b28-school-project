35edf240910173663ec19159df73dc0d354112d3
Sasha <alexandra.aslobodian@gmail.com>
2023-02-17 19:32:08 -0500

    implemented all methods

--- src/main/java/com/cydeo/service/CourseServiceImpl.java
+++ src/main/java/com/cydeo/service/CourseServiceImpl.java
@@ -1,33 +1,63 @@
 package com.cydeo.service;
 
+import com.cydeo.database.Database;
 import com.cydeo.entity.Course;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+
+import static com.cydeo.database.Database.courseList;
 
 public class CourseServiceImpl implements CRUDService<Course> {
 
     @Override
     public Course findById(int id) {
-        return null;
+
+        return courseList.stream()
+                .filter(course -> course.getId() == id)
+                .findFirst()
+                .orElseThrow(() -> new NoSuchElementException("Provided course id: " + id + " is not in the database"));
     }
 
     @Override
     public List<Course> findAll() {
-        return null;
+        return courseList;
     }
 
     @Override
     public void save(Course course) {
+        courseList.add(course);
 
     }
 
     @Override
     public void update(Course course) {
 
+        boolean idInTheDatabase = false;
+        for (int i = 0; i < courseList.size(); i++) {
+            if (courseList.get(i).getId() == course.getId()){
+                courseList.set(i,course);
+                idInTheDatabase = true;
+            }
+
+        }
+        if (!idInTheDatabase)
+            throw new NoSuchElementException("Provided course id: " + course.getId() + " is not in the database");
+
     }
 
     @Override
     public void deleteById(int id) {
 
+        boolean idInTheDatabase = false;
+        for (Course course : courseList) {
+            if (course.getId() == id)
+                Database.courseList.remove(course);
+            idInTheDatabase = true;
+
+        }
+        if (!idInTheDatabase)
+            throw new NoSuchElementException("Provided course id: " + id + " is not in the database");
     }
+
 }
